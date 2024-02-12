1725ca3f41509d9a84c13db523868ab19bc27d19
lordofthesun <94736527+lordofthesun@users.noreply.github.com>
2024-02-12 17:50:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-window.addEventListener('scroll', function() {
-  var header = document.getElementById('header');
-  var scrollPosition = window.scrollY || document.documentElement.scrollTop;
 
-  if (scrollPosition > 100) {
-    header.classList.add('fixed');
-    header.classList.add('title');
-  } else {
-    header.classList.remove('fixed');
-    header.classList.remove('title');
-  }
-});
